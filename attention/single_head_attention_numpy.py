b3641aa53a30b7acd156633de566691e70194112
Royston Soares <roystonsoares@Mac.attlocal.net>
2025-06-07 18:59:26 -0700

    update single head attention file

---
+++ attention/single_head_attention_numpy.py
@@ -0,0 +1,71 @@
+import torch 
+import math
+import numpy as np 
+from typing import Optional
+
+class SingleHeadedAttention:
+    def __init__(self, Q, K, V, d_model: int =512):
+        """
+        Initializes an instace of SingleHeadAttention
+
+        Args:
+            Q (np.ndarry): Query Matrix
+            K (np.ndarry): Key Matrix
+            V (np.ndarry): Val Matrix
+            d_model
+        """
+        self.Q = Q
+        self.K = K
+        self.V = V 
+        self.d_model = d_model
+
+        self.d_k = self.d_model # In multi-headed attention this would be self.d_model/self.num_heads
+        self.attention_mat = None 
+        self.attention_score = None 
+    
+    def softmax_across_row(self, X) -> np.ndarray:
+        """
+        We will use this method to compute the softmax along rows of the input.
+        Formuls of Softmax = exp(x)/ sum(exp(x))
+        """
+        exp_x = np.exp(X)
+        row_sums = np.sum(exp_x, axis = 1, keepdims=True)
+        soft = exp_x/row_sums
+        return soft 
+    
+
+    def compute(self, mask: Optional[np.ndarray] = None) -> np.ndarray:
+        """
+        Compute Single Headed attention (masker or unmaked)
+
+        
+        -- 
+        """
+        qk_final = np.matmul(self.Q, self.K.T) / math.sqrt(self.d_k)
+        if mask is not None:
+            qk_final = qk_final + mask 
+        
+        # Compute Attention Matrix 
+        self.attention_mat= self.softmax_across_row(qk_final)
+
+        # Compute Attention Score 
+        self.attention_score = np.matmul(self.attention_mat, self.V)
+
+        return self.attention_mat, self.attention_score
+
+
+
+
+if __name__ == "__main__":
+    seq_len = 6
+    d_model = 512 
+
+    Q = np.random.randn(seq_len, d_model)
+    K = np.random.randn(seq_len, d_model)
+    V = np.random.randn(seq_len, d_model)
+
+    attention = SingleHeadedAttention(Q = Q, K= K, V=V, d_model=d_model)
+    
+    attention_matrix, final_val = attention.compute()
+    print(attention_matrix)
+        
